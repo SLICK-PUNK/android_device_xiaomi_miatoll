96742eb32c03ea99bfba7114c5cd04e5d04efb15
Sairam <tggsairamchari666@gmail.com>
2022-12-17 19:56:53 +0530

    fix branch in vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/slick-punk/android_device_xiaomi_sm6250-common -b yaap device/xiaomi/sm6250-common
+git clone https://github.com/slick-punk/android_device_xiaomi_sm6250-common -b 13 device/xiaomi/sm6250-common
 git clone https://github.com/slick-punk/android_vendor_xiaomi_sm6250-common -b 13 vendor/xiaomi/sm6250-common
 git clone https://github.com/slick-punk/android_vendor_xiaomi_miatoll -b 13 vendor/xiaomi/miatoll
 git clone --depth=1 https://github.com/ProjectVelvet/android_kernel_sm6250 -b main kernel/xiaomi/sm6250
